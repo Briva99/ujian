76b4b70dedd343f0fcf473498de4a850a9776b6e
Briva99 <bang.briva@gmail.com>
2021-02-20 11:25:09 +0700

    update geolocation dan camera

--- index.js
+++ index.js
@@ -6,5 +6,6 @@ import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 import Adduser from './screens/AddUser/Adduser';
+import Photo from './screens/Photo/Photo';
 
-AppRegistry.registerComponent(appName, () => Adduser);
+AppRegistry.registerComponent(appName, () => Photo);
